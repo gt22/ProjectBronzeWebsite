2cb798a9f31e5755023fb1672662d21f6b07458f
gt22 <igorengel@mail.ru>
2016-05-08 18:56:44 +0300

    улучшена читаемость js файла (При чтении желательно на html синтаксис перейти), исправлен поехавший виджет дискорда на странице файлов

--- script.js
+++ script.js
@@ -7,17 +7,65 @@
 
 function setupMenu()
 {
-	$('.menu').html('<div class="row"><div class="menuback"><div class="menuc"><a class="link" href="multigaming.html"><img class="menuimg" src="images/multigaming.jpg"></a></div><div class="menuc"><a class="link" href="index.html"><img class="menuimg" src="images/main.png"></a></div><div class="menuc"><a class="link" href="file.html"><img class="menuimg" src="images/main.png"></a></div></div></div><div class="row"><div class="menuback"><div class="menuc"><a class="link" href="contacts.html"><img class="menuimg" src="images/contacts.png"></a></div><div class="menuc"><a class="link" href="modpacks.html"><img class="menuimg" src="images/mopacks.png"></a></div><div class="menuc"><a class="link" href="modlists.html"><img class="menuimg" src="images/mopacks.png"></a></div></div></div>')
+	$('.menu').html(`
+	<div class="row">
+		<div class="menuback">
+			<div class="menuc">
+				<a class="link" href="multigaming.html">
+					<img class="menuimg" src="images/multigaming.jpg">
+				</a>
+			</div>
+			<div class="menuc">
+				<a class="link" href="index.html">
+					<img class="menuimg" src="images/main.png">
+				</a>
+			</div>
+			<div class="menuc">
+				<a class="link" href="file.html">
+					<img class="menuimg" src="images/main.png">
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="menuback">
+			<div class="menuc">
+			<a class="link" href="contacts.html">
+				<img class="menuimg" src="images/contacts.png">
+			</a>
+			</div>
+			<div class="menuc">
+				<a class="link" href="modpacks.html">
+					<img class="menuimg" src="images/mopacks.png">
+				</a>
+			</div>
+			<div class="menuc">
+				<a class="link" href="modlists.html">
+				</a>
+					<img class="menuimg" src="images/mopacks.png">
+				</a>
+			</div>
+		</div>
+	</div>`)
 }
 
 function setupHeader()
 {
-	$('#header').html('<a href="index.html"><img src="images/gear.png" width="100px"></a><button class="menubutton" onclick="openmenu()">Menu</button><button id="hidewidgets"onclick="hidewidgets()">Hide widgets</button>')
+	$('#header').html(`
+	<a href="index.html">
+		<img src="images/gear.png" width="100px">
+	</a>
+	<button class="menubutton" onclick="openmenu()">
+		Menu
+	</button>
+	<button id="hidewidgets"onclick="hidewidgets()">
+		Hide widgets
+	</button>`)
 }
 
 function openmenu() 
 {
-$( ".menu" ).last().addClass( "show" );
+$( ".menu" ).last().addClass( 'show' );
 }
 
 var i=1;
