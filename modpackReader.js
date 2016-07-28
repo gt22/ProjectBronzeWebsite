04dfe2732ac682b20847b66efe69cac744a21624
gt22 <igorengel@mail.ru>
2016-07-28 03:46:17 +0300

    Репозиторий обновлён до актуального состояния

---
+++ modpackReader.js
@@ -0,0 +1,14 @@
+$(document).ready(function()
+{
+	setupModpacks();
+})
+
+function setupModpacks()
+{
+	$.getJSON('http://projectbronze.comli.com/launcher/new.json', function(data) {
+        $.each(data.query, function(key, val) {
+			alert(val.name);
+			alert(val.mcversion);
+		})
+    });
+}
